17ea8c7089caa2b3b6961c197ef34781a6bf3ad9
SoyMordekay <76791823+SoyMordekay@users.noreply.github.com>
2023-10-03 19:32:59 -0500

    Creacion de testing de calculadora

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+B
+src/Main/Main.java,5\d\5d88ecaa2385c6ca3ecc3bf87a82796206d0a916
+F
+src/Test/MainTest.java,3\3\33af07ce7b8f5a99809044ca64b19b6c2b2760da
--- src/Test/MainTest2.java
+++ src/Test/MainTest2.java
@@ -1,4 +1,23 @@
+package Test;
+
 import static org.junit.Assert.*;
+import Main.Main;
+import org.junit.Test;
+
 public class MainTest2 {
-  
+
+
+    Main mt = new Main();
+
+    @org.junit.Test
+    public void testsuma() {
+
+        assertEquals(2.0,mt.suma(1,1),0);
+    }
+
+
+   @Test
+   public void testsresta(){
+        assertEquals(2.0,mt.resta(3,1),0);
+    }
 }
